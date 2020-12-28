0e4fe4acadaf26770e23e164fd45ab8e4442253a
wienerr <38580174+wienerr@users.noreply.github.com>
2020-12-29 08:49:47 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+basic.forever(function () {
+    basic.showString("HELLO")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
